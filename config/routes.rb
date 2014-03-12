2bc438e88cea9b5ca9dec2252cbc1aa7311605fc
Indra Bagus Wicaksono <sonny8988@gmail.com>
2014-03-12 15:27:13 +0900

    E-reader can cycle through words

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Ereader::Application.routes.draw do
+  post 'reader' => 'reader#index'
+  root 'load_book#new'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
